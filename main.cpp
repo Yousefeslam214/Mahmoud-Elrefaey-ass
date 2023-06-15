55c424b63e160a66560d5e49c2bcba4b8ab14249
yousefeslam <117038006+Yousefeslam214@users.noreply.github.com>
2023-06-15 20:52:56 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Fig. 3.1: AccountTest.cpp
+// Creating and manipulating an Account object.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+       int count;
+       int num;
+       cin>>num;
+       while (num<=count)
+       {
+              cou<<"*";
+              count++;
+       };
+       
+}
