60de17f58e32df62ec32404a0c7631f35d3aa234
yousefeslam <117038006+Yousefeslam214@users.noreply.github.com>
2023-06-15 20:03:34 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,10 +7,39 @@ using namespace std;
 
 int main() {
        double money;
-       cout<<"enter EGP money: ";
+       double rial;
+       double dollar;
+       double euore;
+       char currency;
+       cout<<"how much EGP you want transform: ";
        cin>>money;
-       money=money/33.7;
-       cout<<"The amount in $ USD is: "<<money;
+       cout<<"what corrency you want to be transformed ";
+       cout<<"r =>rial ,d=>dollar ,e =>euore : ";
+       cin>>currency;
+       if (currency == 'r' ) {
+              cout<<"how much rial equal: ";
+              cin>>rial;
+              money=money/rial;
+       }else if(currency == 'd') {
+              cout<<"how much dollar equal: ";
+              cin>>dollar;
+              money=money/dollar;
+       } else if(currency == 'e'){
+              cout<<"how much euore equal: ";
+              cin>>euore;
+              money=money/euore;
+       } else{
+              cout<<"wrong character";
+              return 0;
+       }
+
+       
+       cout<<"The amount is: "<<money<<"\n";
+       if(money <= 5000){
+              cout<<"you under line of poor";
+       } else{
+              cout<<"you are rich" ;
+       }
        return 0;
 
 
