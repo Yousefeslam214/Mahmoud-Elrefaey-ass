76d64e02e6f4db336df0fd0a2df1dd8d0aaea03b
yousefeslam <117038006+Yousefeslam214@users.noreply.github.com>
2023-06-15 18:36:22 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Fig. 3.1: AccountTest.cpp
+// Creating and manipulating an Account object.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+       double money;
+       cout<<"enter EGP money: ";
+       cin>>money;
+       money=money/33.7;
+       cout<<"The amount in $ USD is: "<<money;
+       return 0;
+
+
+}
